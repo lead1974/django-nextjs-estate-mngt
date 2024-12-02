2505d4b0be4e794fb96358a56bd1a5af91768493
lead1974 <email2lead@gmail.com>
2024-12-02 06:29:59 -0800

    chroe: split django settings for easy management

---
+++ config/settings/local.py
@@ -0,0 +1,29 @@
+from os import getenv, path
+from dotenv import load_dotenv
+from .base import *  # noqa
+from .base import BASE_DIR
+
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+
+DEBUG = True
+
+SITE_NAME = getenv("SITE_NAME")
+
+# SECRET_KEY = 'django-insecure-*r(&(j0gx2n@4ba7f(03zp5_x*s#rb$qcizeka^9&kpzj7yz!m'
+# python -c "import secrets; print(secrets.token_urlsafe(38))"
+SECRET_KEY = getenv(
+    "DJANGO_SECRET_KEY", "MqIe7HAxrkN_OI6IUm8giBY1yA9VYAlf1AN2DU8v19qlYhEuaOE"
+)
+
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+ADMIN_URL = getenv("DJANGO_ADMIN_URL")
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
+DOMAIN = getenv("DOMAIN")
--- readme.txt
+++ readme.txt
@@ -1,3 +1,8 @@
+# pipenv
+pipenv -rm 
+pipenv install
+pipenv graph
+
 # generate .gitignore file
 npx gitignore python
 #initialize git
@@ -5,3 +10,23 @@ git init -b main
 
 # start django project
 django-admin startproject config .
+git config --global user.email "email2lead@gmail.com"
+git config --global user.name "lead1974"
+
+pipenv install --dev psycopg2-binary==2.9.9 watchfiles==0.21.0 black==24.3.0
+
+# building requirements
+mkdir -p requirements && touch requirements/{base,local,production}.txt
+mkdir -p config/settings && touch config/settings/{__init__,base,local,production}.py
+
+# generate secret key
+python -c "import secrets; print(secrets.token_urlsafe(38))"
+
+# ensure env variable are activated
+pip install --upgrade setuptools
+
+# start django server
+python manage.py runserver
+
+
+
