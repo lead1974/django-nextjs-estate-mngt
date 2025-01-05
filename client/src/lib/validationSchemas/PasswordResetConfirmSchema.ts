397de08018daae7e9313843a4b566ed83ed3b7ab
lead1974 <email2lead@gmail.com>
2025-01-05 14:26:11 -0800

    @106 password reset

--- client/src/lib/validationSchemas/PasswordResetConfirmSchema.ts
+++ client/src/lib/validationSchemas/PasswordResetConfirmSchema.ts
@@ -6,10 +6,12 @@ export const passwordResetConfirmSchema = z
 		token: z.string().trim(),
 		new_password: z
 			.string()
-			.min(8, { message: "Password must be at least 8 characters long" }),
-		re_new_password: z.string().min(8, {
-			message: "Confirm Password must be at least 8 characters long",
-		}),
+			.min(1, { message: "Password is required" })
+			.min(4, { message: "Password must be at least 4 characters long" }),
+		re_new_password: z
+			.string()
+			.min(1, { message: "Confirm password is required" })
+			.min(4, { message: "Confirm password must be at least 4 characters long" }),
 	})
 	.refine((data) => data.new_password === data.re_new_password, {
 		message: "Passwords do not match",
