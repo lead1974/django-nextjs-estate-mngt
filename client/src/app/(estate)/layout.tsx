7dd5634eb6c1686a4ae940eed40f28ef325ab44f
lead1974 <email2lead@gmail.com>
2025-01-12 13:56:45 -0800

    @ section 28 is complete

--- client/src/app/(estate)/layout.tsx
+++ client/src/app/(estate)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftNavbar from "@/components/shared/navbar/LeftNavBar";
 import Navbar from "@/components/shared/navbar/Navbar";
+import RightNavbar from "@/components/shared/navbar/RightNavbar";
 import React from "react";
 
 interface LayoutProps {
@@ -16,10 +17,7 @@ export default function layout({ children }: LayoutProps) {
 				<section className="flex min-h-screen flex-1 flex-col px-4 pb-6 pt-24 sm:px-6 lg:px-8 lg:pt-32">
 					<div>{children}</div>
 				</section>
-				{/* placeholder right navbar component */}
-				<div className="dark:text-pumpkin hidden text-xl md:block">
-					Right Navbar
-				</div>
+				<RightNavbar />
 			</div>
 		</main>
 	);
